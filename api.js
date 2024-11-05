bbaabcf7fcfdd9d94792f4aff604eb4d908da9b3
Henry Gressmann <mail@henrygressmann.de>
2024-11-05 14:56:56 +0100

    chore: update snippet
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- api.js
+++ api.js
@@ -1,37 +1,72 @@
 // please use a different api key for your own projects - I don't want this to get rate limited by last.fm
 const apikey = "375d07a53fd83a6e2a8456f9f124fb7d";
 
-export const getData = async ({user, mode, period, amount, filter}) => {
-	user = encodeURIComponent(user)
+export const getData = async ({ user, mode, period, amount, filter }) => {
+	user = encodeURIComponent(user);
 
-	const resp = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettop${mode}&user=${user}&period=${period}&limit=${parseInt(amount) + (mode === "artists" ? 20 : 0)}&api_key=52e8e86c171ed9affffa34580666927a&format=json`)
+	const resp = await fetch(
+		`https://ws.audioscrobbler.com/2.0/?method=user.gettop${mode}&user=${user}&period=${period}&limit=${Number.parseInt(amount) + (mode === "artists" ? 20 : 0)}&api_key=52e8e86c171ed9affffa34580666927a&format=json`,
+	);
 	const data = await resp.json();
 
 	if (mode === "artists") {
-		return await Promise.all(data.topartists.artist.filter(artist => {
-			if (!filter) {
-				return !artist.name.includes(", ")
-			};
-			return true;
-		}).slice(0, amount).map(async artist => {
-			const name = encodeURIComponent(artist.name);
-			const data = await(await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${name}&api_key=52e8e86c171ed9affffa34580666927a&format=json`)).json()
-			return Promise.resolve([artist.name, data.artist ? Number(data.artist.stats.listeners) : 0, artist.url])
-		}));
+		return await Promise.all(
+			data.topartists.artist
+				.filter((artist) => {
+					if (!filter) {
+						return !artist.name.includes(", ");
+					}
+					return true;
+				})
+				.slice(0, amount)
+				.map(async (artist) => {
+					const name = encodeURIComponent(artist.name);
+					const data = await (
+						await fetch(
+							`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${name}&api_key=52e8e86c171ed9affffa34580666927a&format=json`,
+						)
+					).json();
+					return Promise.resolve([
+						artist.name,
+						data.artist ? Number(data.artist.stats.listeners) : 0,
+						artist.url,
+					]);
+				}),
+		);
 	}
 
 	if (mode === "albums")
-		return await Promise.all(data.topalbums.album.map(async album => {
-			const name = encodeURIComponent(album.name);
-			const artist = encodeURIComponent(album.artist.name);
-			const data = await(await fetch(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&album=${name}&artist=${artist}&api_key=52e8e86c171ed9affffa34580666927a&format=json`)).json()
-			return Promise.resolve([album.artist.name + ' – ' + album.name, data.album ? Number(data.album.listeners) : 0, album.url])
-		}));
+		return await Promise.all(
+			data.topalbums.album.map(async (album) => {
+				const name = encodeURIComponent(album.name);
+				const artist = encodeURIComponent(album.artist.name);
+				const data = await (
+					await fetch(
+						`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&album=${name}&artist=${artist}&api_key=52e8e86c171ed9affffa34580666927a&format=json`,
+					)
+				).json();
+				return Promise.resolve([
+					`${album.artist.name} – ${album.name}`,
+					data.album ? Number(data.album.listeners) : 0,
+					album.url,
+				]);
+			}),
+		);
 
 	if (mode === "tracks")
-		return await Promise.all(data.toptracks.track.map(async track => {
-			const name = encodeURIComponent(track.artist.name);
-			const data = await(await fetch(`https://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist=${name}&track=${encodeURIComponent(track.name)}&api_key=${apikey}&format=json`)).json()
-			return Promise.resolve([track.artist.name + ' – ' + track.name, data.track ? Number(data.track.listeners) : 0, track.url])
-		}));
-}
+		return await Promise.all(
+			data.toptracks.track.map(async (track) => {
+				const name = encodeURIComponent(track.artist.name);
+				const data = await (
+					await fetch(
+						`https://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist=${name}&track=${encodeURIComponent(track.name)}&api_key=${apikey}&format=json`,
+					)
+				).json();
+				return Promise.resolve([
+					`${track.artist.name} – ${track.name}`,
+					data.track ? Number(data.track.listeners) : 0,
+					track.url,
+				]);
+			}),
+		);
+};
--- index.js
+++ index.js
@@ -2,86 +2,99 @@ import { getData } from "./api.js";
 import { toJpeg } from "https://esm.sh/html-to-image@1.11.11";
 
 if (document.location.host === "explodingcamera.github.io") {
-  document.location = "https://lastfm-iceberg.dawdle.space";
+	document.location = "https://lastfm-iceberg.dawdle.space";
 }
 
 const downloadJPG = () => {
-  const el = document.getElementById('results');
+	const el = document.getElementById("results");
 
-  toJpeg(el, { quality: 0.95 })
-    .then((dataUrl) => {
-        const link = document.createElement('a');
-        link.download = 'iceberg.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
-}
+	toJpeg(el, { quality: 0.95 }).then((dataUrl) => {
+		const link = document.createElement("a");
+		link.download = "iceberg.jpeg";
+		link.href = dataUrl;
+		link.click();
+	});
+};
 
 const setError = (error) => {
-  document.getElementById("submit").disabled = false;
-  document.getElementById("message").style.display = "none";
-  document.getElementById("download").style.display = "none";
-  document.getElementById("error").innerHTML = `Error: ${error}.\nTry disabling adblockers and refreshing the page, some adblockers block the last.fm API.`;
-  document.getElementById("error").style.display = "block";
+	document.getElementById("submit").disabled = false;
+	document.getElementById("message").style.display = "none";
+	document.getElementById("download").style.display = "none";
+	document.getElementById("error").innerHTML =
+		`Error: ${error}.\nTry disabling adblockers and refreshing the page, some adblockers block the last.fm API.`;
+	document.getElementById("error").style.display = "block";
 };
 
 const setSubmitted = () => {
-  document.getElementById("submit").disabled = true;
-  document.getElementById("message").style.display = "block";
-  document.getElementById("download").style.display = "none";
-  document.getElementById("error").style.display = "none";
-  document.getElementById("results").style.display = "none";
+	document.getElementById("submit").disabled = true;
+	document.getElementById("message").style.display = "block";
+	document.getElementById("download").style.display = "none";
+	document.getElementById("error").style.display = "none";
+	document.getElementById("results").style.display = "none";
 };
 
 const setSuccess = () => {
-  document.getElementById("submit").disabled = false;
-  document.getElementById("message").style.display = "none";
-  document.getElementById("results").style.display = "block";
-  document.getElementById("download").style.display = "block";
+	document.getElementById("submit").disabled = false;
+	document.getElementById("message").style.display = "none";
+	document.getElementById("results").style.display = "block";
+	document.getElementById("download").style.display = "block";
 };
 
 const handleSubmit = async (e) => {
-  e.preventDefault();
-  setSubmitted();
-  
-  for (const result of document.getElementsByClassName("result")) result.innerHTML = "";
+	e.preventDefault();
+	setSubmitted();
 
-  const options = Object.fromEntries(
-    new FormData(document.getElementById("form")).entries()
-  );
+	for (const result of document.getElementsByClassName("result"))
+		result.innerHTML = "";
 
-  let data;
-  try {
-    data = await getData(options);
-  } catch (error) {
-    setError(error.message);
-    return
-  }
+	const options = Object.fromEntries(
+		new FormData(document.getElementById("form")).entries(),
+	);
 
-  data = data.sort((x, y) => x[1] - y[1]).reverse();
-  data = data.map((x) => [x[0], x[1] - Math.min(...data.map((x) => x[1])), x[2]]);
-  data = data.map((x) => [x[0], x[1] / Math.max(...data.map((x) => x[1])), x[2]]);
-  data = data.map((x) => [x[0], 8 - x[1] * 8, x[2]]);
-  data = data.map((x, i) => [x[0], Math.round((x[1] + (i / data.length) * 24) / 4), x[2]]);
+	let data;
+	try {
+		data = await getData(options);
+	} catch (error) {
+		setError(error.message);
+		return;
+	}
 
-  for (const element of data) {
-    if (Number.isNaN(element[1])) continue;
-     
-    const result = document.getElementsByClassName("result")[element[1]];
-    const text = document.createElement("a");
-    text.appendChild(document.createTextNode(element[0]));
-    text.setAttribute("href", element[2]);
-    result.appendChild(text);
-  }
+	data = data.sort((x, y) => x[1] - y[1]).reverse();
+	data = data.map((x) => [
+		x[0],
+		x[1] - Math.min(...data.map((x) => x[1])),
+		x[2],
+	]);
+	data = data.map((x) => [
+		x[0],
+		x[1] / Math.max(...data.map((x) => x[1])),
+		x[2],
+	]);
+	data = data.map((x) => [x[0], 8 - x[1] * 8, x[2]]);
+	data = data.map((x, i) => [
+		x[0],
+		Math.round((x[1] + (i / data.length) * 24) / 4),
+		x[2],
+	]);
 
-  for (const element of document.getElementsByClassName("result")) element.innerHTML = element.innerHTML.trim();
-  setSuccess();
+	for (const element of data) {
+		if (Number.isNaN(element[1])) continue;
+
+		const result = document.getElementsByClassName("result")[element[1]];
+		const text = document.createElement("a");
+		text.appendChild(document.createTextNode(element[0]));
+		text.setAttribute("href", element[2]);
+		result.appendChild(text);
+	}
+
+	for (const element of document.getElementsByClassName("result"))
+		element.innerHTML = element.innerHTML.trim();
+	setSuccess();
 };
 
 window.addEventListener("load", () => {
-  document.getElementById("form").addEventListener("submit", handleSubmit);
+	document.getElementById("form").addEventListener("submit", handleSubmit);
 
-
-  const button = document.getElementById("download");
-  button.addEventListener("click", downloadJPG);
+	const button = document.getElementById("download");
+	button.addEventListener("click", downloadJPG);
 });
